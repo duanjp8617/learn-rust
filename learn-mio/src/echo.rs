e3b42e4b1840463e5540fbca1c393caf5968cb37
djp <djp@djps-MacBook-Pro-2.local>
2019-12-02 23:58:37 +0800

    update

--- learn-mio/src/echo.rs
+++ learn-mio/src/echo.rs
@@ -1,13 +1,31 @@
-struct CmdPat<T, F: FnMut(&mut T, String)> {
+use std::vec::Vec;
+use std::marker::PhantomData;
+
+struct CmdPat<T, F: Fn(&mut T, String)> {
     pat : String,
     need_arg : bool,
     visited : bool,
     op : F,
-    marker : std::marker::PhantomData<T>,
+    marker : PhantomData<T>,
 }
 
-struct DummyCliParser<T> {
+struct DummyCliParser<T, F: Fn(&mut T, String)> {
     cli_info : T,
+    pats : Vec<CmdPat<T, F>>,
+}
+
+impl<T, F: Fn(&mut T, String)> DummyCliParser<T, F> {
+    pub fn new(default : T) -> DummyCliParser<T, F> {
+        DummyCliParser {
+            cli_info : default,
+            pats : Vec::new(),
+        }
+    }
+
+    pub fn register_cmd_pat(&mut self, pat : String, need_arg : bool, op : F) {
+        
+    }
+
 
 }
 
